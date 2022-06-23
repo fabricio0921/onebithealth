2a8f27d23a4b22a4d2145e9c9e3d88580850bb33
fabricio0921 <fabricioestudo@gmail.com>
2022-06-22 22:51:53 -0300

    Projeto Finalizado

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,9 +1,13 @@
-import React, {useState} from "react";
-import {View,
-     Text,
+import React, { useState } from "react";
+import {
+    View,
+    Text,
     TextInput,
     TouchableOpacity,
     Vibration,
+    Pressable,
+    Keyboard,
+    FlatList
 
 } from "react-native";
 
@@ -13,82 +17,114 @@ import styles from "./style";
 
 
 
-export default function Form(){
-const [height, setHeight]= useState(null)
-const [weight, setWeight]= useState(null)
-const [messageImc, setMessageImc]= useState("preencha o peso e a altura")
-const [imc, setImc]= useState(null)
-const [textButton, setTextButton]= useState("Calcular")
-const [errorMessage, setErrorMessage] = useState(null)
+export default function Form() {
+    const [height, setHeight] = useState(null)
+    const [weight, setWeight] = useState(null)
+    const [messageImc, setMessageImc] = useState("preencha o peso e a altura")
+    const [imc, setImc] = useState(null)
+    const [textButton, setTextButton] = useState("Calcular")
+    const [errorMessage, setErrorMessage] = useState(null)
+    const [imcList, setImcList] = useState([])
 
 
-function verificatioImc() {
-    if (imc==null) {
-        Vibration.vibrate()
-        setErrorMessage('Campo obrigatório')
-        
+    function verificatioImc() {
+        if (imc == null) {
+            Vibration.vibrate()
+            setErrorMessage('Campo obrigatório')
+
+        }
+
     }
-    
-}
 
 
-function imcCalculator() {
-    return setImc((weight/(height*height)).toFixed(2))
-    
-}
-
-function validationImc() {
-    if(weight != null && height != null){
-        imcCalculator()
-        setHeight(null)
-        setWeight(null)
-        setMessageImc("Seu imc é igual: ")
-        setTextButton("Calcular Novamente")
-        setErrorMessage(null)
-
-        return
+    function imcCalculator() {
+        let heightFormart = height.replace(",", ".")
+        let totalImc = ((weight / (heightFormart * heightFormart)).toFixed(2))
+        setImcList((arr) => [...arr, { id: new Date().getTime(), imc: totalImc }])
+        setImc(totalImc)
     }
-    verificatioImc() 
-    setImc(null)
-    setTextButton('Calcular')
-    setMessageImc('preencha Peso e Altura')
-    
-    
-}
 
-    return(
+    function validationImc() {
+        console.log(imcList)
+        if (weight != null && height != null) {
+            imcCalculator()
+            setHeight(null)
+            setWeight(null)
+            setMessageImc("Seu IMC é igual: ")
+            setTextButton("Calcular Novamente")
+            setErrorMessage(null)
+        } else {
+            verificatioImc()
+            setImc(null)
+            setTextButton('Calcular')
+            setMessageImc('preencha Peso e Altura')
+        }
+
+
+
+    }
+
+    return (
+
         <View style={styles.formContext}>
-            <View style={styles.form}>
+            {imc == null ?
+                <Pressable style={styles.form} onPress={Keyboard.dismiss}>
+                    <Text style={styles.formLabel}>Altura</Text>
+                    <Text style={styles.errorMessage}>{errorMessage}</Text>
+                    <TextInput
+                        style={styles.input}
+                        onChangeText={setHeight}
+                        value={height}
+                        placeholder="Ex. 1.75"
+                        keyboardType="numeric"
+                    />
+                    <Text style={styles.formLabel}>Peso</Text>
+                    <Text style={styles.errorMessage}>{errorMessage}</Text>
 
-                <Text style={styles.formLabel}>Altura</Text>
-                <Text style={styles.errorMessage}>{errorMessage}</Text>
-                <TextInput
-                style={styles.input}
-                onChangeText={setHeight}
-                value={height}
-                placeholder="Ex. 1.75"
-                keyboardType="numeric"
-                />
-                <Text style={styles.formLabel}>Peso</Text>
-                <Text style={styles.errorMessage}>{errorMessage}</Text>
+                    <TextInput
+                        style={styles.input}
+                        onChangeText={setWeight}
+                        value={weight}
+                        placeholder="Ex. 72.236"
+                        keyboardType="numeric"
+                    />
 
-                <TextInput
-                style={styles.input}
-                onChangeText={setWeight}
-                value={weight}
-                placeholder="Ex. 72.236"
-                keyboardType="numeric"
-                />
+                    <TouchableOpacity style={styles.buttonCalculator} onPress={() => validationImc()}>
+                        <Text style={styles.textButtonCalculator}>{textButton}</Text>
 
-                <TouchableOpacity style={styles.buttonCalculator} onPress={()=>validationImc()}>
-                    <Text style={styles.textButtonCalculator}>{textButton}</Text>
-                    
-                </TouchableOpacity>
-               
+                    </TouchableOpacity>
 
-            </View>
-            <ResultImc messageResultImc={messageImc} resultImc={imc}/>
-            
+
+                </Pressable>
+                :
+                <View style={styles.exhibitionResultImc}>
+                    <ResultImc messageResultImc={messageImc} resultImc={imc} />
+                    <TouchableOpacity style={styles.buttonCalculator} onPress={() => validationImc()}>
+                        <Text style={styles.textButtonCalculator}>{textButton}</Text>
+
+                    </TouchableOpacity>
+
+                </View>
+            }
+            <FlatList
+            showsVerticalScrollIndicator={false}
+                style={styles.listImcs}
+                data={imcList.reverse()}
+                renderItem={({ item }) => {
+                    return (
+                        <Text style={styles.resultImcItem}>
+                            <Text style={styles.TextResultItemList}>Resultado IMC = </Text>
+                            {item.imc}
+                        </Text>
+
+
+                    )
+                }}
+                keyExtractor={(item) => { item.id }}
+            />
+
+
+
         </View>
     )
 }
--- src/components/Form/style.js
+++ src/components/Form/style.js
@@ -1,60 +1,77 @@
 import React from "react";
 import reactDom from "react-dom";
-import {View, Text, StyleSheet} from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 
 
 
 const styles = StyleSheet.create({
     formContext: {
+        flex: 1,
+        backgroundColor: '#ffffff',
+        
+        alignItems: 'center',
+        paddingTop: 30,
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30
+    },
+    form: {
+        width: "100%",
+        
+        
+    },
+    formLabel: {
+        color: "#000000",
+        fontSize: 18,
+        paddingLeft: 20
+    },
+    input: {
+        width: "90%",
+        borderRadius: 50,
+        backgroundColor: "#f6f6f6",
+        height: 40,
+        margin: 12,
+        paddingLeft: 10
+    },
+    buttonCalculator: {
+        borderRadius: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        width: "90%",
+        backgroundColor: "#ff0043",
+        paddingTop: 14,
+        paddingBottom: 14,
+        marginLeft: 12,
+        margin: 30
+    },
+    textButtonCalculator: {
+        fontSize: 20,
+        color: "#ffffff"
+    },
+    errorMessage: {
+        fontSize: 12,
+        color: 'red',
+        fontWeight: "bold",
+        paddingLeft: 20,
+    },
+    exhibitionResultImc:{
         width:"100%",
-        height:'100%',
-        bottom:0,
-        backgroundColor:'#ffffff',
-        alignItems:'center',
-        marginTop:30,
-        borderTopLeftRadius:30,
-        borderTopRightRadius:30
+        height:'50%'
     },
-    form:{
-        width:"100%",
-        height:"auto",
-        marginTop:30,
-        padding:10
+    listImcs:{
+marginTop:20
     },
-    formLabel:{
-        color:"#000000",
-        fontSize:18,
-        paddingLeft:20
-    },
-    input:{
-        width:"90%",
-        borderRadius:50,
-        backgroundColor:"#f6f6f6",
-        height:40,
-        margin:12,
-        paddingLeft:10
-    },
-    buttonCalculator:{
-        borderRadius:50,
-        alignItems:"center",
-        justifyContent:"center",
-        width:"90%",
-        backgroundColor:"#ff0043",
-        paddingTop:14,
-        paddingBottom:14,
-        marginLeft:12,
-        margin:30
-    },
-    textButtonCalculator:{
-        fontSize:20,
-        color:"#ffffff"
-    },
-    errorMessage:{
-        fontSize:12,
+    resultImcItem:{
+        fontSize:26,
         color:'red',
-        fontWeight:"bold",
-        paddingLeft:20,
+        height:50,
+        width:"100%",
+        paddingRight:20
+
+    },
+    TextResultItemList:{
+        color:"red",
+        fontSize:14
     }
 });
 
